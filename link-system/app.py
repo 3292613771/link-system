a73bc822be978307a9f7bc45c3e4184bce315f5d
3292613771 <3292613771@qq.com>
2026-07-14 20:44:43 +0800

    Update app.py

--- link-system/app.py
+++ link-system/app.py
@@ -315,5 +315,5 @@ def query_mail():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
