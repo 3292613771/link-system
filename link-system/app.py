78161a3e3e4d401a9a25ef05fc35b7b63a673577
3292613771 <3292613771@qq.com>
2026-07-15 22:32:04 +0800

    Update app.py

--- link-system/app.py
+++ link-system/app.py
@@ -510,6 +510,12 @@ def auto_create_link():
     save_links(links)
     
     link_url = f"https://{DOMAIN}/query?link={link_id}"
+
+    return f"""您购买的邮箱已发货
+
+邮箱：{selected_emails[0]}
+查询链接：{link_url}
+有效期至：{links[link_id]['expire_at']}"""
     
 @app.route('/query')
 def query_page():
